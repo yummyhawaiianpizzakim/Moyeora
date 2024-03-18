b6580d1c2a29073da507bef8f7fbcd359a96d806
yummyhawaiianpizzakim <yummyhawaiianpizzakim@gmail.com>
2024-03-18 15:18:38 +0900

    외부 의존성 추가

--- Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -5,6 +5,17 @@ public extension TargetDependency {
 }
 
 public extension TargetDependency.SPM {
+    static let RxSwift = TargetDependency.external(name: "RxSwift")
+    static let RxCocoa = TargetDependency.external(name: "RxCocoa")
+    static let RxRelay = TargetDependency.external(name: "RxRelay")
+    static let RxGesture = TargetDependency.external(name: "RxGesture")
+    static let SnapKit = TargetDependency.external(name: "SnapKit")
+    static let KingFisher = TargetDependency.external(name: "KingFisher")
+    static let FirebaseAuth = TargetDependency.external(name: "FirebaseAuth")
+    static let FirebaseFirestore = TargetDependency.external(name: "FirebaseFirestore")
+    static let FirebaseFirestoreSwift = TargetDependency.external(name: "FirebaseFirestoreSwift")
+    static let FirebaseStorage = TargetDependency.external(name: "FirebaseStorage")
+    
 }
 
 public extension Package {
--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -5,7 +5,11 @@ let dependencies = Dependencies(
     carthage: nil,
     swiftPackageManager: SwiftPackageManagerDependencies(
         [
-    
+            .remote(url: "https://github.com/ReactiveX/RxSwift.git", requirement: .upToNextMajor(from: "6.6.0")),
+            .remote(url: "https://github.com/RxSwiftCommunity/RxGesture.git", requirement: .upToNextMajor(from: "4.0.0")),
+            .remote(url: "https://github.com/firebase/firebase-ios-sdk.git", requirement: .upToNextMajor(from: "10.22.0")),
+            .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMajor(from: "5.7.0")),
+            .remote(url: "https://github.com/onevcat/Kingfisher", requirement: .upToNextMajor(from: "7.0"))
         ],
         baseSettings: .settings(
             configurations: [
